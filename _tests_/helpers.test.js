97f234e0a6983b22cb1b5e65e5a8173d354e3aed
ktodoran <ktodoran@gmail.com>
2021-09-20 18:15:22 -0400

    Updated API Files

--- _tests_/helpers.test.js
+++ _tests_/helpers.test.js
@@ -1,5 +1,8 @@
 const { format_date } = require('../utils/helpers');
+
 test('format_date() returns a date string', () => {
-    const date = new Date('2021-09-10 13:12:03');
-    expect(format_date(date)).toBe('9/10/2021');
+    
+    const date = new Date('2021-09-12 16:12:03');
+    
+    expect(format_date(date)).toBe('9/12/2021');
 });
---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,82 @@
+const router = require('express').Router();
+const  { Comment } = require('../../models');
+const withAuthorization = require('../../utils/auth');
+
+    router.get('/', (req, res) => {
+        Comment.findAll({})
+            .then(dbCommentsData => res.json(dbCommentsData))
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+        })
+});
+
+    router.get('/:id', (req, res) => {
+        Comment.findAll({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(dbCommentsData => res.json(dbCommentsData))
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err);
+        })
+});
+
+    router.post('/', withAuthorization, (req, res) => {
+        if(req.session) {
+            Comment.create({
+                commentText: req.body.commentText,
+                postId: req.body.post_id,
+                userId: req.session.user_id,
+            })
+            .then(dbCommentsData => res.json(dbCommentsData))
+            .catch(err => {
+                console.log(err)
+                res.status(400).json(err);
+        })
+    }
+});
+
+    router.put('/:id', withAuthorization, (req, res) => {
+        Comment.update({
+            commentTExt: req.body.commentText,
+        }, {
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(dbCommentsData => {
+            if(!dbCommentsData) {
+                res.status(404).json({ message: 'No comment associated with this id'});
+                return;
+            }
+            res.json(dbCommentsData);
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err);
+    });
+});
+
+    router.delete('/:id', withAuthorization, (req, res) => {
+        Comment.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(dbCommentsData => {
+            if(!dbCommentsData) {
+                res.status(404).json({ message: 'No comment associated with this id'});
+                return;
+            }
+            res.json(dbCommentsData);
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err);
+        });
+    });
+
+    module.exports = router;
